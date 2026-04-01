4ef0ec69fd12f693adc739a326f22dd09fa27b62
mayara <mayara.soliveiraifc@gmail.com>
2026-04-01 15:05:09 -0300

    FEAT(vite + logo): configurando o vite.config.js + logo

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,30 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     vue(),
-    
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Padaria Santo Martins',
+        short_name: 'Santo Martins',
+        description: 'Sistema de Encomendas e Cardápio da Padaria Santo Martins',
+        theme_color: '#3D2B1F',
+        background_color: '#F2E8CF',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
     vueDevTools(),
   ],
   resolve: {
