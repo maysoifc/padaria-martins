2e46272eba7a00fa02439256b8a8dac36fab3dc4
mayara <mayara.soliveiraifc@gmail.com>
2026-04-09 11:05:21 -0300

    FEAT(main.js): importando registro SW

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/global.css'
+
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
