edb965d04b788bcc9108df53c7ac43afdf7d5426
mayara <mayara.soliveiraifc@gmail.com>
2026-04-13 12:41:36 -0300

    FEAT(ProductCard.vue): modelo de cartão de produtos-template

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  produto: Object
+});
+</script>
+
+<template>
+  <div class="card">
+  <img src="" alt="Produto"> <h3>{{ produto.nome_produto  }}</h3>
+  <p>R$ {{ produto.preco }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
