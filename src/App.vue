2c1fc17fe896039409575ee7e3791916258fc450
mayara <mayara.soliveiraifc@gmail.com>
2026-04-13 12:34:47 -0300

    FEAT:(App.vue): importando os components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue';
+import MenuView from './views/MenuView.vue';
+import ProductDetail from './views/ProductDetail.vue';
+import ProfileView from './views/ProfileView.vue';
+import AppHeader from './components/AppHeader.vue';
+import BottomBav from './components/BottomBav.vue';
+import CategoryBar from './components/CategoryBar.vue';
+import ProductCard from './components/ProductCard.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -18,6 +24,7 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <HomeView />
 </template>
 
 <style scoped>
