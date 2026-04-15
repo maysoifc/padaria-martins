b73ddf5b6643a94bdf313ee0dc39378bf14ed8b7
mayara <mayara.soliveiraifc@gmail.com>
2026-04-15 13:51:13 -0300

    FEAT(ProductCard): informações dos produtos(cartão)

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -6,11 +6,38 @@ defineProps({
 
 <template>
   <div class="card">
-  <img src="" alt="Produto"> <h3>{{ produto.nome_produto  }}</h3>
-  <p>R$ {{ produto.preco }}</p>
+    <img :src="produto.imagem" :alt="produto.nome_produto">
+    <h3>{{ produto.nome_produto }}</h3>
+    <p class="preco">R$ {{ produto.preco }}</p>
   </div>
 </template>
 
 <style scoped>
+.card {
+  background-color:  #fff;
+  border-radius: 15px;
+  padding: 12px;
+  text-align: center;
+  box-shadow: 0.4px 6px rgba(0, 0, 0, 0.05);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.card img {
+  width: 100%;
+  border-radius: 10px;
+  object-fit: cover;
+  aspect-ratio:  1/1;
+}
 
+h3{
+  color: #3d2b1f;
+  font-size: 0.9rem;
+  margin: 8.px 0 4px;
+}
+
+.preco {
+  color: #8B5E3C;
+  font-weight: bold;
+}
 </style>
