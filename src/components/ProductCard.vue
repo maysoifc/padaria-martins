b75082fffbb8536cd147b5ff3d5efcac5864d89e
mayara <mayara.soliveiraifc@gmail.com>
2026-04-16 11:21:10 -0300

    FEAT(ProductCard): Css do component ProductCard

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -39,5 +39,59 @@ const props = defineProps({
 </template>
 
 <style scoped>
+.card {
+  background-color:  #fff;
+  border-radius: 30px;
+  padding-bottom: 20px;
+  text-align: center;
+  box-shadow: 0.4px 15px rgba(0, 0, 0, 0.05);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card:active {
+  transform: translate(-5px) scale(0.98);
+  box-shadow: 0 5px 10px rgb(0, 0, 0, 0.1);
+}
+.card img {
+  width:  100%;
+  height: 140px;
+  border-radius: 30px 30px 0 0;
+  object-fit: cover;
+  aspect-ratio:  1/1;
+}
+
+h3{
+  color: #6a6059;
+  font-size: 1.2rem;
+  font-weight: bolder;
+  text-align: center;
+  margin-top: 12px;
+  margin-bottom: 5px;
+}
+
+.preco {
+  color: #bba270;
+  font-weight: bold;
+  font-size: 1.3rem;
+  margin-top: 12px;
+}
+
+.favorite-btn{
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 1.5rem;
+  padding: 0;
+  font-size: 1.8rem;
+}
+
+.border-top{
+  display: flex;
+  justify-content: space-between;
+  border-top: 3px solid #bba270;
+  width: 80%;
+}
 
 </style>
