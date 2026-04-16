f7fed670d8b82f246e125480474cb60b1cdcf7d3
mayara <mayara.soliveiraifc@gmail.com>
2026-04-16 12:59:35 -0300

    FEAT(Promocoes): continuação do css

--- src/components/Promocoes.vue
+++ src/components/Promocoes.vue
@@ -23,8 +23,62 @@ const promocoes = [
   </section>
 </template>
 <style scoped>
+.titulo-sessao{
+  color: #5E3023;
+  padding: 20px 16px 10px;
+  font-family: 'Imprima';
+  font-weight: bold;
+  text-align: center;
+  font-size: 43px;
+  margin: 0;
+}
+.grade-promocoes {
+  padding: 20px 16px;
+}
+
+.grade-promocoes {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+  max-width: 500px;
+  margin: 0 auto;
+}
 .promo-card {
   background-color: #f5f5f5;
+  border-radius: 40px;
+  text-align: center;
+  box-shadow: 0 4px 10px;
 }
+
+.promo-img {
+  width: 100%;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 20px 20px 0 0;
+}
+
+.promo-info {
+  padding: 12px 8px 25px;
+}
+
+.promo-info h4 {
+  color: #545050;
+  font-size: 1.3rem;
+  margin-bottom: 8px;
+  font-weight: bold;
+  margin-top: 0;
+  font-family: 'Inter';
+}
+
+.dia-promo {
+  background-color: #D9CAB3;
+  color:#545050;
+  padding: 5px 12px;
+  border-radius: 15px;
+  font-size: 0.8rem;
+  display: inline-block;
+  font-weight: bolder;
+}
+
 </style>
 
