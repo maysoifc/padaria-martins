39c2dc339e9c4703a21820f9e4503a4ca1600cc1
mayara <mayara.soliveiraifc@gmail.com>
2026-04-16 11:20:34 -0300

    FEAT(ProdutCard): Adicionara botão favoritar

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,43 +1,43 @@
 <script setup>
-defineProps({
+const props = defineProps({
   produto: Object
 });
+
+ const toggleFavorito = async() => {
+    const novoStatus = !props.produto.favorito;
+
+    try {
+      const response = await fetch(`http://localhost:3000/produtos/${props.produto.idProduto}/favorito`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ favorito: novoStatus })
+      });
+
+      if (response.ok) {
+        props.produto.favorito = novoStatus;
+      }
+    }catch (error) {
+      console.error("Erro ao favoritar", error);
+      alert("Não foi possível favoritar o produto.");
+    }
+  };
 </script>
 
 <template>
   <div class="card">
     <img :src="produto.imagem" :alt="produto.nome_produto">
     <h3>{{ produto.nome_produto }}</h3>
+    <div class="border-top">
     <p class="preco">R$ {{ produto.preco }}</p>
+     <button class="favorite-btn" @click.stop="toggleFavorito">
+  <i :class="produto.favorito ? 'fas fa-heart' : 'far fa-heart'"
+     :style="{ color: produto.favorito ? '#bba270' : '#bba270' }">
+  </i>
+</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.card {
-  background-color:  #fff;
-  border-radius: 15px;
-  padding: 12px;
-  text-align: center;
-  box-shadow: 0.4px 6px rgba(0, 0, 0, 0.05);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.card img {
-  width: 100%;
-  border-radius: 10px;
-  object-fit: cover;
-  aspect-ratio:  1/1;
-}
 
-h3{
-  color: #3d2b1f;
-  font-size: 0.9rem;
-  margin: 8.px 0 4px;
-}
-
-.preco {
-  color: #8B5E3C;
-  font-weight: bold;
-}
 </style>
