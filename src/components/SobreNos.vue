84604705391638ea8eee25a498bba729e45e3368
mayara <mayara.soliveiraifc@gmail.com>
2026-04-17 08:57:14 -0300

    FEAT(SobreNos): templete sobre nos

---
+++ src/components/SobreNos.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="historia-container">
+  <h2 class="titulo-historia">Nossa História: Tradição e amor em cada fornada</h2>
+
+  <div class="historia-conteudo">
+    <div class="moldura-foto">
+    <img src="/img/historia.jpeg" alt="Padaria Martins" class="foto-historia">
+    </div>
+
+    <div class="linha-vertical"></div>
+    <div class="texto-historia">
+    <p>
+    A Padaria Martins nasceu do sonho de levar o pão artesanal à mesa de cada vizinho.
+    Com receitas de gerações, honramos o cuidado no preparo e a escolha dos melhores ingredientes.
+    Cada fornada é um compromisso com o seu café da manhã.
+    </p>
+    </div>
+  </div>
+  </section>
+</template>
