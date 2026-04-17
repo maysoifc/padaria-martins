103f8136d94099bf8989913303fc2972f4cb45ba
mayara <mayara.soliveiraifc@gmail.com>
2026-04-17 10:26:54 -0300

    FEAT(BottomNav): templete component BottomNav

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,32 @@
+<script>
+</script>
+<template>
+  <nav class="bottom-nav">
+     <router-link to="/" class="nav-item">
+     <i class="fa-solid fa-house"></i>
+        <span>Home</span>
+    </router-link>
+
+    <router-link to="/menu" class="nav-item">
+        <i class="fa-solid fa-list-check"></i>
+        <span>Cardápio</span>
+    </router-link>
+
+    <router-link to="/perfil" class="nav-item">
+    <i class="fa-solid fa-user"></i>
+        <span>Perfil</span>
+    </router-link>
+
+    <router-link to="/carrinho" class="nav-item">
+    <i class="fa-solid fa-cart-shopping"></i>
+        <span>Carrinho</span>
+    </router-link>
+
+    <router-link to="/mapa" class="nav-item">
+    <i class="fa-solid fa-location-dot"></i>
+        <span>Mapa</span>
+    </router-link>
+  </nav>
+</template>
+<style>
+</style>
