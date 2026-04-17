6b9e3df2c9ff096c8fef240687c3234caaa193f7
mayara <mayara.soliveiraifc@gmail.com>
2026-04-17 08:34:06 -0300

    FEAT(AppHeader): css AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,5 +17,60 @@
   </header>
 </template>
 <style scoped>
+.banner {
+  width: 100%;
+  height:  250px;
+  background-image: url('/img/banner/banner.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+}
 
+.banner-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 84%;
+  background-color: rgba(0, 0, 0, 0.4);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+}
+
+.banner-content {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  max-width: 600px;
+  width: 100%;
+  justify-content: flex-start;
+}
+
+.banner-logo {
+  flex-shrink: 0;
+  margin-top: 0;
+  margin-right: 20px;
+  height: 80px;
+}
+
+.banner-texts {
+  color: #FFFFFF;
+  font-family: 'Times New Roman';
+}
+
+.banner-titulo {
+  font-size: 3rem;
+  margin: 0;
+  font-weight: 500;
+  line-height: 1.1;
+}
+
+.banner-slogan {
+  font-size: 1.5rem;
+  margin-top: 12px;
+  font-weight: bolder;
+}
 </style>
