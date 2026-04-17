11afd53bf228c6ecd1b16eb5ea5d13270fcc4fa0
mayara <mayara.soliveiraifc@gmail.com>
2026-04-17 08:33:34 -0300

    FEAT(AppHeader): Templete AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,21 @@
 <script setup>
 </script>
 <template>
-  
+  <header class="banner">
+  <div class="banner-overlay">
+    <div class="banner-content">
+      <div class="banner-logo">
+      <img src="/img/banner/logo-paes.png" alt="Logo" class="banner-logo">
+      </div>
+
+      <div class="banner-texts">
+      <h1 class="banner-titulo">Padaria Martins</h1>
+      <p class="banner-slogan">O pão nosso</p>
+      </div>
+    </div>
+  </div>
+  </header>
 </template>
+<style scoped>
+
+</style>
