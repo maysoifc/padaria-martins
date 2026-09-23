5782f4233ad8a00e019a39db9f32a4023b841c8e
mayara <mayara.soliveiraifc@gmail.com>
2026-09-23 15:18:50 -0300

    feat(SelosCarteiraView): component SelosCarteiraView.vue completo

---
+++ src/views/SelosCarteiraView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import SelosCarteira from "@/components/SelosCarteira.vue";
+</script>
+
+<template>
+  <div class="selos-page">
+    <SelosCarteira />
+  </div>
+</template>
+
+<style scoped>
+.selos-page {
+  min-height: 100vh;
+  background: #f8f4ed;
+}
+</style>
